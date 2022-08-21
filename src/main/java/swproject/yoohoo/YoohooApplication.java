a3078b6feeda775c6d69e2f6cc3c6991b5a37aa7
hanb613 <60910288+hanb613@users.noreply.github.com>
2022-08-22 01:03:50 +0900

    chore : 프로젝트 생성

---
+++ src/main/java/swproject/yoohoo/YoohooApplication.java
@@ -0,0 +1,13 @@
+package swproject.yoohoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoohooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoohooApplication.class, args);
+	}
+
+}
