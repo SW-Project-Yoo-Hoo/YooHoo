2dfa9a3708aab7a75b3963c2a480b237645f14ec
hanb613 <60910288+hanb613@users.noreply.github.com>
2022-08-29 17:01:11 +0900

    chore : app.jsx

--- yoohoo/src/App.jsx
+++ yoohoo/src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./app.module.css";
+import Header from "./components/header/header";
 import Home from "./components/page/home/home";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <Header />
+    </>
+  );
 }
 
 export default App;
