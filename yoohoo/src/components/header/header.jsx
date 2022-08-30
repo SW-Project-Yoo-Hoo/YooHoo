34b2b68bf2a966ea5c0f4db288f85f7153bdc53e
guswn2209@gmail.com <guswn2209@gmail.com>
2022-08-30 21:49:21 +0900

    add : Material Icons

--- yoohoo/src/components/header/header.jsx
+++ yoohoo/src/components/header/header.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import styles from "./header.module.css";
-import Logo from "./leftLogo/leftLogo.jsx";
 import RightComponent from "./rightComponent/rightComponent.jsx";
 
 const Header = (props) => {
   return (
     <header className={styles.header}>
-      {/* left component */}
+      {/* left component : logo */}
       <div className={styles.logo}>
-        <Logo />
+        <img
+          src="/Images/header/logo.png"
+          style={{ width: 100, height: 30 }}
+        ></img>
       </div>
 
-      {/* rigth component */}
+      {/* rigth component : pages, search, alram, profile */}
       <div className={styles.rightComponent}>
         <RightComponent />
       </div>
--- yoohoo/src/components/header/rightComponent/user/user.jsx
+++ yoohoo/src/components/header/rightComponent/user/user.jsx
@@ -3,10 +3,11 @@ import styles from "./user.module.css";
 
 const User = (props) => {
   return (
-    <>
-      user
-      {/* search,alarm,profile */}
-    </>
+    <user>
+      <img src="/Images/header/profile.png"></img>
+      <img src="/Images/header/alert.png"></img>
+      <img src="/Images/header/search.png"></img>
+    </user>
   );
 };
 
