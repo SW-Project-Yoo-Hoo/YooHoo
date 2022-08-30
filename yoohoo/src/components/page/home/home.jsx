9f82e6730809045a8f4aed216768ddfe68d9d0b5
guswn2209@gmail.com <guswn2209@gmail.com>
2022-08-30 20:00:36 +0900

    add : 헤더 하위 파일 추가

--- yoohoo/src/components/page/home/home.jsx
+++ yoohoo/src/components/page/home/home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import styles from "./home.module.css";
+import Header from "../../header/header";
+
+const Main = () => {
+  <img src="/Images/home/background.png"></img>;
+};
 
 const Home = (props) => {
-  return <img src="/Images/home/background.png"></img>;
-  //<img src="/ImageName.jpg">
+  return <Header />;
 };
 
 export default Home;
