2b27c64be6743c5786f5137e93d8bacabbe9cbed
hanb613 <yeoongs2@gmail.com>
2022-08-30 19:53:25 +0900

    feat : Footer 수정 및 FAQ 페이지 연결

--- yoohoo/src/components/footer/footer.jsx
+++ yoohoo/src/components/footer/footer.jsx
@@ -5,23 +5,26 @@ import Notice from "./notice/notice";
 
 const Footer = (props) => {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.notice}>
-        <span className={styles.noticeTitle}>Notice</span>
-        <Notice />
-        <span className={styles.logo}>ⓒ Yoohoo</span>
-      </div>
+    <>
+      <hr className={styles.footerLine} />
+      <footer className={styles.footer}>
+        <div className={styles.notice}>
+          <span className={styles.noticeTitle}>Notice</span>
+          <Notice />
+          <span className={styles.logo}>ⓒ Yoohoo</span>
+        </div>
 
-      <div>
-        <p className={styles.infoTitle}>DONG-A UNIVERSITY</p>
-        <p className={styles.dept}>Department of Computer Engineering</p>
-      </div>
+        <div>
+          <p className={styles.infoTitle}>DONG-A UNIVERSITY</p>
+          <p className={styles.dept}>Department of Computer Engineering</p>
+        </div>
 
-      <div>
-        <span className={styles.aboutTitle}>About Us</span>
-        <AboutUs />
-      </div>
-    </footer>
+        <div>
+          <span className={styles.aboutTitle}>About Us</span>
+          <AboutUs />
+        </div>
+      </footer>
+    </>
   );
 };
 
--- yoohoo/src/components/footer/notice/notice.jsx
+++ yoohoo/src/components/footer/notice/notice.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import FAQ from "./faq";
 import styles from "./notice.module.css";
 
@@ -6,12 +7,16 @@ const Notice = (props) => {
   return (
     <div>
       <ul className={styles.list}>
-        <li className={styles.item}>FAQ</li>
+        <Link to="/faq">
+          <li className={styles.item}>FAQ</li>
+        </Link>
+
         <li className={styles.item}>
           <a href="https://github.com/SW-Project-Yoo-Hoo" target="_blank">
             Connect Us - Notion
           </a>
         </li>
+
         <li className={styles.item}>
           <a href="https://github.com/SW-Project-Yoo-Hoo" target="_blank">
             Connect Us - GitHub
