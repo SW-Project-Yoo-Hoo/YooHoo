f197ad01e665b36652522d5fffd77a6ed9e35a4e
hanb613 <yeoongs2@gmail.com>
2022-08-30 19:51:35 +0900

    add : react-router-dom

--- yoohoo/src/app.jsx
+++ yoohoo/src/app.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./app.module.css";
 import Footer from "./components/footer/footer";
+import FAQ from "./components/footer/notice/faq";
 import Header from "./components/header/header";
 import Home from "./components/page/home/home";
 
 function App() {
   return (
-    <>
-      <Home />
-      <Header />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/faq" element={<FAQ />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
