6e37f96df4a260945f34eede21553dab94f8d543
hanb613 <yeoongs2@gmail.com>
2022-09-12 19:52:17 +0900

    refactor : Route 수정

--- yoohoo/src/app.jsx
+++ yoohoo/src/app.jsx
@@ -4,14 +4,19 @@ import "./app.module.css";
 import FAQ from "./components/footer/notice/faq";
 import About from "./components/page/about/about";
 import Home from "./components/page/home/home";
+import Post from "./components/page/post/post";
+import Shop from "./components/page/shop/shop";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" exact="true" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
         <Route path="/About" element={<About />} />
-        <Route path="/faq" element={<FAQ />} />
+        <Route path="/Faq" element={<FAQ />} />
+        <Route path="/Shop" element={<Shop />} />
+        <Route path="/Post" element={<Post />} />
       </Routes>
     </BrowserRouter>
   );
