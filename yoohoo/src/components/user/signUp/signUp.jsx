1faaf78985facc8e30411087e40f88b521de5d2a
guswn2209@gmail.com <guswn2209@gmail.com>
2022-09-12 17:45:39 +0900

    add : 회원 정보 형식 확인 기능 구현

--- yoohoo/src/components/user/signUp/signUp.jsx
+++ yoohoo/src/components/user/signUp/signUp.jsx
@@ -15,6 +15,9 @@ const SignUp = (props) => {
 
   const { id, pw, companyName, adress, phone } = inputs;
 
+  const [incorrect, setIncorrect] = useState(false);
+  const [alertText, setAlertText] = useState("");
+
   //회원 정보 입력 핸들링
   const changeHandling = (e) => {
     const { value, name } = e.target;
@@ -24,21 +27,58 @@ const SignUp = (props) => {
     });
   };
 
-  const [incorrect, setIncorrect] = useState(false);
-
-  //아이디 정규표현식
-  const idRegex = () => {};
-
-  //비밀번호 정규표현식
-  const pwRegex = () => {};
+  //회원가입 정보 정규식
+  const Regex = {
+    idRegex:
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z\.])+[a-zA-Z]{2,3}$/i,
+    pwRegex: /(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,14}$/,
+    phoneRegex: /^\d{2,3}-\d{3,4}-\d{4}$/,
+  };
 
   const signUpHandling = () => {
+    setIncorrect(false);
     //백엔드로 회원 정보 전송하기
-    //만약 회원가입이 제대로 됐다면 로그인 페이지로 이동
-    // window.location.href = "/Login";
 
-    //만약 회원가입이 제대로 안됐다면(중복된 아이디) 알림 띄우기
-    setIncorrect(true);
+    //만약 회원가입이 제대로 됐다면 로그인 페이지로 이동
+    window.location.href = "/Login";
+    //만약 중복된 아이디라면 알림 띄우기
+    //setAlertText("중복된 아이디입니다.");
+  };
+
+  const regexHandling = () => {
+    //정규식 확인하고 일치하지 않으면 알림 띄우기
+    if (id.match(Regex.idRegex) === null) {
+      setIncorrect(true);
+      setAlertText("부적잘한 이메일입니다.");
+    } else if (pw.match(Regex.pwRegex) === null) {
+      setIncorrect(true);
+      setAlertText("부적잘한 비밀번호 형식입니다.");
+    } else if (phone.match(Regex.phoneRegex) === null) {
+      setIncorrect(true);
+      setAlertText("부적잘한 연락처 형식입니다.");
+    } else {
+      //회원정보가 모두 올바르면 signUpHandling 호출
+      signUpHandling();
+    }
+  };
+
+  const UserInfoHandling = () => {
+    //회원정보 확인하기
+    if (
+      id === "" ||
+      pw === "" ||
+      companyName === "" ||
+      adress === "" ||
+      phone === ""
+    ) {
+      //모든 정보가 입력 되지 않았다면 알림 띄우기
+      setIncorrect(true);
+      setAlertText("모든 정보가 입력되지 않았습니다.");
+    } else {
+      //회원정보가 모두 입력 되었으면 regexHandling 호출
+      setIncorrect(false);
+      regexHandling();
+    }
   };
 
   return (
@@ -61,14 +101,14 @@ const SignUp = (props) => {
           {/* id */}
           <div className={styles.form}>
             <label className={styles.label}>
-              <sapn>아이디</sapn>
+              <sapn>이메일</sapn>
               <span className={styles.labelSpan}> *</span>
               <input
                 type="text"
                 name="id"
                 onChange={changeHandling}
                 value={id}
-                placeholder="영대/소문자, 숫자를 모두 포함한 6~12자리"
+                placeholder="이메일을 입력해주세요"
                 className={styles.input}
               />
             </label>
@@ -84,7 +124,7 @@ const SignUp = (props) => {
                 name="pw"
                 onChange={changeHandling}
                 value={pw}
-                placeholder="영대/소문자, 숫자, 특수문자를 모두 포함한 6~14자리"
+                placeholder="영대/소문자, 숫자를 모두 포함한 6~14자리"
                 className={styles.input}
               />
             </label>
@@ -140,7 +180,7 @@ const SignUp = (props) => {
 
           {/* loginAlert */}
           <div className={incorrect ? styles.alertInfo : styles.alertInfoNone}>
-            <spna className={styles.alertSpan}>중복된 아이디입니다.</spna>
+            <spna className={styles.alertSpan}>{alertText}</spna>
           </div>
 
           {/* signUpButton */}
@@ -151,7 +191,7 @@ const SignUp = (props) => {
           >
             <button
               className={styles.signUpButtonStyle}
-              onClick={signUpHandling}
+              onClick={UserInfoHandling}
             >
               가입하기
             </button>
