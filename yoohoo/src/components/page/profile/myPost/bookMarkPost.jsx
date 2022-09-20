fe9fca3200b6692d6775a1c2ef52e407d52ee813
guswn2209@gmail.com <guswn2209@gmail.com>
2022-09-20 12:09:54 +0900

    design : 게시물이 없을때 레이아웃 구현

--- yoohoo/src/components/page/profile/myPost/bookMarkPost.jsx
+++ yoohoo/src/components/page/profile/myPost/bookMarkPost.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import styles from "./bookMarkPost.module.css";
 
-function editProfile() {
-  return <div>찜!</div>;
-}
+const bookMarkPost = (props) => {
+  const postInfo = {
+    ok: false,
+    img: "",
+    title: "",
+    unit: "",
+    price: "",
+  };
 
-export default editProfile;
+  const getPostInfo = () => {
+    //백엔드에서 정보 가져오기
+    // postInfo.ok = true;
+    return postInfo.ok;
+  };
+
+  return (
+    <div className={styles.container}>
+      {/* 게시물이 없을때 */}
+      <div className={getPostInfo() ? styles.displayNone : styles.noPost}>
+        <span>찜한 게시물이 없습니다</span>
+      </div>
+
+      {/* 게시물이 있을때 */}
+    </div>
+  );
+};
+
+export default bookMarkPost;
