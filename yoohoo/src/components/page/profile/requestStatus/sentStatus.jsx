6f2926c47e6bc361b67d290c2f4692f77716a97b
guswn2209@gmail.com <guswn2209@gmail.com>
2022-09-20 12:18:35 +0900

    design : 게시물이 없을때 레이아웃 구현

--- yoohoo/src/components/page/profile/requestStatus/sentStatus.jsx
+++ yoohoo/src/components/page/profile/requestStatus/sentStatus.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import styles from "./sentStatus.module.css";
 
-function editProfile() {
-  return <div>찜!</div>;
-}
+const SentStatus = (props) => {
+  const postInfo = {
+    ok: false,
+    img: "",
+    title: "",
+    unit: "",
+    price: "",
+  };
 
-export default editProfile;
+  const getPostInfo = () => {
+    //백엔드에서 정보 가져오기
+    // postInfo.ok = true;
+    return postInfo.ok;
+  };
+
+  return (
+    <div className={styles.container}>
+      {/* 게시물이 없을때 */}
+      <div className={getPostInfo() ? styles.displayNone : styles.noPost}>
+        <span>대기 중인 요청이 없습니다</span>
+      </div>
+
+      {/* 게시물이 있을때 */}
+    </div>
+  );
+};
+
+export default SentStatus;
--- yoohoo/src/components/page/profile/tansStatus/completStatus.jsx
+++ yoohoo/src/components/page/profile/tansStatus/completStatus.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import styles from "./completStatus.module.css";
 
-function editProfile() {
-  return <div>찜!</div>;
-}
+const CompletStatus = (props) => {
+  const postInfo = {
+    ok: false,
+    img: "",
+    title: "",
+    unit: "",
+    price: "",
+  };
 
-export default editProfile;
+  const getPostInfo = () => {
+    //백엔드에서 정보 가져오기
+    // postInfo.ok = true;
+    return postInfo.ok;
+  };
+
+  return (
+    <div className={styles.container}>
+      {/* 게시물이 없을때 */}
+      <div className={getPostInfo() ? styles.displayNone : styles.noPost}>
+        <span>완료된 거래가 없습니다</span>
+      </div>
+
+      {/* 게시물이 있을때 */}
+    </div>
+  );
+};
+
+export default CompletStatus;
