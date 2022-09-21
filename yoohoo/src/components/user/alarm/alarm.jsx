059ba7aa874651a3b15ae4aff7aecb1aa1f7bb0b
hanb613 <yeoongs2@gmail.com>
2022-09-21 19:11:06 +0900

    feat : 알람 페이지 구현

--- yoohoo/src/components/user/alarm/alarm.jsx
+++ yoohoo/src/components/user/alarm/alarm.jsx
@@ -1,16 +1,62 @@
 import React from "react";
 import styles from "./alarm.module.css";
-import { Link, NavLink } from "react-router-dom";
 import Header from "../../header/header";
+import Footer from "../../footer/footer";
 
-const Post = (props) => {
+const Alarm = (props) => {
   return (
-    <div className={styles.container}>
+    <>
       <Header />
-      <div className={styles.backGroundIamge} />
-      <h1>알람</h1>
-    </div>
+      <div className={styles.background}>
+        <img
+          className={styles.headerImg}
+          src={process.env.PUBLIC_URL + "images/headerBackground.png"}
+          alt="Header"
+        />
+
+        <div className={styles.container}>
+          <img
+            className={styles.megaphoneImg}
+            src={process.env.PUBLIC_URL + "images/alarm/megaphone.svg"}
+            alt="Megaphone"
+          />
+          <div className={styles.line}>
+            <div className={styles.hr}></div>
+            <div className={styles.circle}></div>
+          </div>
+          <div className={styles.alarm}>
+            <div className={styles.dayContiner}>
+              <img
+                className={styles.timeImg}
+                src={process.env.PUBLIC_URL + "images/alarm/time.svg"}
+                alt="Time"
+              />
+
+              <p className={styles.day}>8월 19일</p>
+            </div>
+
+            <div className={styles.contents}>
+              <div className={styles.pic}>
+                <img
+                  className={styles.productImg}
+                  src={process.env.PUBLIC_URL + "images/shop/a.jpg"}
+                  alt="Product"
+                />
+              </div>
+              <div className={styles.P}>
+                <p className={styles.title}>반갑습니다!</p>
+                <p className={styles.description}>
+                  회원님의 다양한 자원을 공유해보세요.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Footer />
+      </div>
+    </>
   );
 };
 
-export default Post;
+export default Alarm;
