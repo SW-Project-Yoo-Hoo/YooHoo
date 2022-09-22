aea2422fe470a7d60fbb1fe45e42798264d56bed
hanb613 <yeoongs2@gmail.com>
2022-09-22 17:00:16 +0900

    feat : 알람 페이지 구현

--- yoohoo/src/components/user/alarm/alarm.jsx
+++ yoohoo/src/components/user/alarm/alarm.jsx
@@ -2,60 +2,92 @@ import React from "react";
 import styles from "./alarm.module.css";
 import Header from "../../header/header";
 import Footer from "../../footer/footer";
+import AlarmList from "./alarmList";
 
 const Alarm = (props) => {
   return (
-    <>
+    <div className={styles.wrap}>
       <Header />
-      <div className={styles.background}>
+      <img
+        className={styles.headerImg}
+        src={process.env.PUBLIC_URL + "images/headerBackground.png"}
+        alt="Header"
+      />
+      <img
+        className={styles.backImg}
+        src={process.env.PUBLIC_URL + "images/alarm/background.png"}
+        alt="Background"
+      />
+      <div className={styles.alarmList}>
         <img
-          className={styles.headerImg}
-          src={process.env.PUBLIC_URL + "images/headerBackground.png"}
-          alt="Header"
+          className={styles.megaphoneImg}
+          src={process.env.PUBLIC_URL + "images/alarm/megaphone.svg"}
+          alt="Megaphone"
         />
-
-        <div className={styles.container}>
-          <img
-            className={styles.megaphoneImg}
-            src={process.env.PUBLIC_URL + "images/alarm/megaphone.svg"}
-            alt="Megaphone"
-          />
-          <div className={styles.line}>
-            <div className={styles.hr}></div>
-            <div className={styles.circle}></div>
+        {AlarmList.map((item, index) => (
+          <div className={styles.alarmItems} key={item.id}>
+            {index === 0 || item.date !== AlarmList[index - 1].date ? (
+              <>
+                <div>
+                  <div className={styles.hr}></div>
+                  <div className={styles.circle}></div>
+                </div>
+                <div className={styles.containDate}>
+                  <div className={styles.alarm}>
+                    <div className={styles.dayContiner}>
+                      <img
+                        className={styles.timeImg}
+                        src={process.env.PUBLIC_URL + "images/alarm/time.svg"}
+                        alt="Time"
+                      />
+                      <p className={styles.day}>{item.date}</p>
+                    </div>
+                    <div className={styles.contents}>
+                      <div className={styles.pic}>
+                        <img
+                          className={styles.productImg}
+                          src={item.image}
+                          alt="Product"
+                        />
+                      </div>
+                      <div className={styles.P}>
+                        <p className={styles.title}>{item.title}</p>
+                        <p className={styles.description}>{item.content}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className={styles.noneDate}>
+                  <div className={styles.contour}>
+                    <div className={styles.hr2}></div>
+                    <div className={styles.circle2}></div>
+                  </div>
+                  <div className={styles.alarm}>
+                    <div className={styles.contents2}>
+                      <div className={styles.pic}>
+                        <img
+                          className={styles.productImg}
+                          src={item.image}
+                          alt="Product"
+                        />
+                      </div>
+                      <div className={styles.P}>
+                        <p className={styles.title}>{item.title}</p>
+                        <p className={styles.description}>{item.content}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            )}
           </div>
-          <div className={styles.alarm}>
-            <div className={styles.dayContiner}>
-              <img
-                className={styles.timeImg}
-                src={process.env.PUBLIC_URL + "images/alarm/time.svg"}
-                alt="Time"
-              />
-
-              <p className={styles.day}>8월 19일</p>
-            </div>
-
-            <div className={styles.contents}>
-              <div className={styles.pic}>
-                <img
-                  className={styles.productImg}
-                  src={process.env.PUBLIC_URL + "images/shop/a.jpg"}
-                  alt="Product"
-                />
-              </div>
-              <div className={styles.P}>
-                <p className={styles.title}>반갑습니다!</p>
-                <p className={styles.description}>
-                  회원님의 다양한 자원을 공유해보세요.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <Footer />
+        ))}
+        <Footer className={styles.footer} />
       </div>
-    </>
+    </div>
   );
 };
 
