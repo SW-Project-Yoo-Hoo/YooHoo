b0abf2e39d4d02cfef8c237c84adf6428f2cf84b
hanb613 <yeoongs2@gmail.com>
2022-10-04 17:23:44 +0900

    feat : 알람 페이지 백엔드 연동

--- yoohoo/src/components/user/alarm/alarm.jsx
+++ yoohoo/src/components/user/alarm/alarm.jsx
@@ -10,42 +10,46 @@ const Alarm = (props) => {
   const [alarmList, setAlarmList] = useState([]);
 
   // 마이프로필로 이동하기 위한 call 추가
-  AlarmList.map((item) => {
-    switch (item.title) {
+  function setCall(title) {
+    switch (title) {
       case "반납 일정":
-        item.call = "ProgressStatus";
-        break;
+        return "ProgressStatus";
       case "반납 요청":
-        item.call = "ProgressStatus";
-        break;
+        return "ProgressStatus";
       case "조기 반납 요청":
-        item.call = "ProgressStatus";
-        break;
+        return "ProgressStatus";
       case "거래 취소":
-        item.call = "SentStatus";
-        break;
+        return "SentStatus";
       case "거래 시작":
-        item.call = "ProgressStatus";
-        break;
+        return "ProgressStatus";
       case "거래 요청":
-        item.call = "ReceivedStatus";
-        break;
+        return "ReceivedStatus";
       case "거래 완료":
-        item.call = "CompletStatus";
-        break;
+        return "CompletStatus";
       default:
-        item.call = "MyPost";
+        return "MyPost";
     }
-  });
+  }
 
   /* 백엔드에서 알람 리스트 가져오기 */
   useEffect(() => {
     const getList = () => {
       axios
         .get("/alarms")
-        .then((res) => setAlarmList(res.data.data))
+        .then((res) => {
+          setAlarmList(
+            res.data.data.map((item, index) => ({
+              title: item.title,
+              content: item.content,
+              dateMonth: new Date(item.alarmDate).getMonth() + 1,
+              dateDate: new Date(item.alarmDate).getDate(),
+              call: setCall(item.title),
+            }))
+          );
+        })
         .catch((error) => console.log(error));
     };
+
     getList();
   }, []);
 
@@ -59,91 +63,109 @@ const Alarm = (props) => {
             src={process.env.PUBLIC_URL + "images/headerBackground.png"}
             alt="Header"
           />
+
           <div className={styles.alarmList}>
-            {alarmList.map((item, index) => (
-              <div className={styles.alarmItems} key={item.id}>
-                {index === 0 && (
-                  <img
-                    className={styles.megaphoneImg}
-                    src={process.env.PUBLIC_URL + "images/alarm/megaphone.svg"}
-                    alt="Megaphone"
-                  />
-                )}
-                {index === 0 || item.date !== AlarmList[index - 1].date ? (
-                  <>
-                    <div>
-                      <div
-                        className={
-                          item.title === "반갑습니다!"
-                            ? styles.hrFirst
-                            : styles.hr
-                        }
-                      ></div>
-                      <div className={styles.circle}></div>
-                    </div>
-                    <div className={styles.containDate}>
-                      <div className={styles.alarm}>
-                        <div className={styles.dayContiner}>
-                          <img
-                            className={styles.timeImg}
-                            src={
-                              process.env.PUBLIC_URL + "images/alarm/time.svg"
-                            }
-                            alt="Time"
-                          />
-                          <p className={styles.day}>{item.alarmDate}</p>
-                        </div>
-                        <Link to="/profile" state={{ call: item.call }}>
-                          <div className={styles.contents}>
-                            <div className={styles.pic}>
-                              <img
-                                className={styles.productImg}
-                                src={item.image}
-                                alt="Product"
-                              />
-                            </div>
-                            <div className={styles.P}>
-                              <p className={styles.title}>{item.title}</p>
-                              <p className={styles.description}>
-                                {item.content}
-                              </p>
-                            </div>
-                          </div>
-                        </Link>
-                      </div>
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <div className={styles.noneDate}>
+            {alarmList &&
+              alarmList.reverse().map((item, index) => (
+                <div className={styles.alarmItems} key={item.id}>
+                  {index === 0 && (
+                    <img
+                      className={styles.megaphoneImg}
+                      src={
+                        process.env.PUBLIC_URL + "images/alarm/megaphone.svg"
+                      }
+                      alt="Megaphone"
+                    />
+                  )}
+
+                  {index === 0 ||
+                  (item.dateMonth === alarmList[index - 1].dateMonth &&
+                    item.dateDate !== alarmList[index - 1].dateDate) ? (
+                    <>
                       <div>
-                        <div className={styles.hr2}></div>
-                        <div className={styles.circle2}></div>
+                        <div
+                          className={
+                            item.title === "반갑습니다!"
+                              ? styles.hrFirst
+                              : styles.hr
+                          }
+                        ></div>
+                        <div className={styles.circle}></div>
                       </div>
-                      <div className={styles.alarm}>
-                        <Link to="/profile" state={{ call: item.call }}>
-                          <div className={styles.contents2}>
-                            <div className={styles.pic}>
-                              <img
-                                className={styles.productImg}
-                                src={item.image}
-                                alt="Product"
-                              />
-                            </div>
-                            <div className={styles.P}>
-                              <p className={styles.title}>{item.title}</p>
-                              <p className={styles.description}>
-                                {item.content}
-                              </p>
-                            </div>
+                      <div className={styles.containDate}>
+                        <div className={styles.alarm}>
+                          <div className={styles.dayContiner}>
+                            <img
+                              className={styles.timeImg}
+                              src={
+                                process.env.PUBLIC_URL + "images/alarm/time.svg"
+                              }
+                              alt="Time"
+                            />
+
+                            <p className={styles.day}>
+                              {item.dateMonth}월 {item.dateDate}일
+                            </p>
                           </div>
-                        </Link>
+                          <Link to="/profile" state={{ call: item.call }}>
+                            <div className={styles.contents}>
+                              <div className={styles.pic}>
+                                {item.title === "반갑습니다!" ? (
+                                  <img
+                                    className={styles.productImg}
+                                    src="/Images/header/logo.png"
+                                    alt="logo"
+                                  />
+                                ) : (
+                                  <img
+                                    className={styles.productImg}
+                                    src={item.image}
+                                    alt="Product"
+                                  />
+                                )}
+                              </div>
+                              <div className={styles.P}>
+                                <p className={styles.title}>{item.title}</p>
+                                <p className={styles.description}>
+                                  {item.content}
+                                </p>
+                              </div>
+                            </div>
+                          </Link>
+                        </div>
                       </div>
-                    </div>
-                  </>
-                )}
-              </div>
-            ))}
+                    </>
+                  ) : (
+                    <>
+                      <div className={styles.noneDate}>
+                        <div>
+                          <div className={styles.hr2}></div>
+                          <div className={styles.circle2}></div>
+                        </div>
+                        <div className={styles.alarm}>
+                          <Link to="/profile" state={{ call: item.call }}>
+                            <div className={styles.contents2}>
+                              <div className={styles.pic}>
+                                <img
+                                  className={styles.productImg}
+                                  src={item.image}
+                                  alt="Product"
+                                />
+                              </div>
+                              <div className={styles.P}>
+                                <p className={styles.title}>{item.title}</p>
+                                <p className={styles.description}>
+                                  {item.content}
+                                </p>
+                              </div>
+                            </div>
+                          </Link>
+                        </div>
+                      </div>
+                    </>
+                  )}
+                </div>
+              ))}
             <Footer className={styles.footer} />
           </div>
         </div>
