4930815e2042d3071d5a29f55d182baea6207fcb
guswn2209@gmail.com <guswn2209@gmail.com>
2022-10-07 01:23:13 +0900

    fix : 로그인 확인 기능 제거

--- yoohoo/src/components/user/login/login.jsx
+++ yoohoo/src/components/user/login/login.jsx
@@ -143,8 +143,9 @@ const Login = (props) => {
 };
 
 const MyPage = (props) => {
-  let test = 0;
-  return test ? <Navigate to="/profile" replace={true} /> : <Login />;
+  // let test = 0;
+  // return test ? <Navigate to="/profile" replace={true} /> : <Login />;
+  return <Login />;
 };
 
 export default MyPage;
