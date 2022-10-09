ae08b35b4ea83c3605d9ab4b2a129dc6242cca05
guswn2209@gmail.com <guswn2209@gmail.com>
2022-10-09 14:25:54 +0900

    feat : 거래 진행 중 조회 기능 구현

--- yoohoo/src/components/page/profile/tansStatus/progressStatus.jsx
+++ yoohoo/src/components/page/profile/tansStatus/progressStatus.jsx
@@ -1,54 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./progressStatus.module.css";
 import { MdPlayCircleFilled } from "react-icons/md";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const ProgressStatus = (props) => {
-  const post = [];
+  const [post, setPost] = useState([]);
 
-  const postInfo1 = {
-    //테스트용 객체
-    id: "게시글 아이디1",
-    img: "/Images/test.jpeg",
-    title: "testTitle이 얼마나 길어질까유쩔죠~~",
-    startDay: "2022.09.28",
-    endDay: "2022.10.22",
-    message: {
-      //현재 상태
-      state: "요청 중",
-      text: "조기반납",
-    },
-
-    showButton: false,
+  //반납,조기반납 버튼 토글 이벤트
+  const onToggle = (id) => {
+    setPost(
+      post.map((post) =>
+        post.deal_id === id ? { ...post, toggle: !post.toggle } : post
+      )
+    );
   };
 
-  const postInfo2 = {
-    //테스트용 객체
-    id: "게시글 아이디2",
-    img: "/Images/home/earth.svg",
-    title: "testTitle이 얼마나 길어질까유쩔죠~~",
-    startDay: "2023.08.22",
-    endDay: "2024.09.22",
-    message: {
-      //현재 상태
-      state: "요청하기",
-      text: "반납",
-    },
-    showButton: false,
-  };
-
-  const getpost = () => {
-    //백엔드에서 정보 가져오기
-    //정보가 존재하면 객체 넣기
-    post.push(postInfo1);
-    post.push(postInfo2);
-  };
-
-  const pageNaviHandling = (props) => {
-    //해당 페이지 상세보기로 이동하기
-    console.log("이동하기!");
-  };
-
+  //반납, 조기반납 핸들링
   const returnButtonShow = (props) => {
+    const text = "반납";
     const returnButton = (event) => {
       //백엔드로 반납 정보 전송
       //현재 상태가 요청 중이면 전송하지 않음
@@ -57,11 +27,17 @@ const ProgressStatus = (props) => {
 
     return (
       <div
+        //백에서 값 받아와서 아래 적용하기
+        // className={[
+        //   styles.returnButtonShow,
+        //   props.message.text === "반납"
+        //     ? styles.returnButtonStyle
+        //     : styles.earlyReturnButton,
+        // ].join(" ")}
+
         className={[
           styles.returnButtonShow,
-          props.message.text === "반납"
-            ? styles.returnButtonStyle
-            : styles.earlyReturnButton,
+          text === "반납" ? styles.returnButtonStyle : styles.earlyReturnButton,
         ].join(" ")}
       >
         {/* 조기반납 */}
@@ -70,40 +46,40 @@ const ProgressStatus = (props) => {
           onClick={(event) => returnButton(event)}
         >
           <span>
-            {props.message.text} {props.message.state}
+            {/* {props.message.text} {props.message.state} */}
+            반납 요청하기
           </span>
         </div>
       </div>
     );
   };
 
-  const ShowPost = (props, id) => {
-    //각 게시글마다 버튼 show 상태 관리
-    const [showButton, setShowButton] = useState(props.showButton);
-
+  const ShowPost = (props) => {
     return (
-      <div
-        className={styles.post}
-        onClick={() => pageNaviHandling(props)}
-        key={id}
-      >
+      <div className={styles.post} key={props.deal_id}>
         {/* 게시물 사진 */}
         <div className={styles.postImgDay}>
-          <img className={styles.postImg} src={props.img} alt="img" />
+          <Link to={`/detail/${props.post_id}`} state={{ info: props }}>
+            <img
+              className={styles.postImg}
+              src={process.env.PUBLIC_URL + "productList/" + props.image}
+              alt="이미지를 찾을 수 없습니다"
+            />
+          </Link>
           {/* button*/}
           <div
             className={styles.returnButton}
             onClick={(event) => {
               //이벤트 버블링 방지
               event.stopPropagation();
-              props.showButton = !showButton;
-              setShowButton(props.showButton);
+              //버튼 토글 이벤트
+              onToggle(props.deal_id);
             }}
           >
             <MdPlayCircleFilled className={styles.iconButton} />
           </div>
           {/* 조기반납, 반납버튼 */}
-          {showButton ? returnButtonShow(props) : ""}
+          {props.toggle ? returnButtonShow(props) : ""}
         </div>
 
         {/* 게시물 제목 */}
@@ -114,30 +90,55 @@ const ProgressStatus = (props) => {
         {/* 게시물 대여 날짜 */}
         <div className={[styles.dealDate, styles.dealDateMargin].join(" ")}>
           <span>시작날짜</span>
-          <span>{props.startDay}</span>
+          <span>{props.startDate}</span>
         </div>
         <div className={styles.dealDate}>
           <span>반납날짜</span>
-          <span>{props.endDay}</span>
+          <span>{props.returnDate}</span>
         </div>
       </div>
     );
   };
 
+  useEffect(() => {
+    let postAdd = [...post];
+    async function get() {
+      await axios
+        .get("/my/myInDeals")
+        .then(function (response) {
+          if (response.data.code === 200) {
+            //데이터 받기 성공
+            let responseData = response.data.data;
+
+            for (const [key, value] of Object.entries(responseData)) {
+              postAdd[key] = {
+                ...value,
+                //버튼 토글 속성 추가
+                toggle: false,
+              };
+            }
+            setPost(postAdd);
+          }
+        })
+        .catch(function (error) {
+          // 오류발생시 실행
+          console.log(error);
+        });
+    }
+    get();
+  }, []);
+
   return (
     <div className={styles.container}>
-      {getpost()}
-      {/* 게시물이 없을때 */}
-      <div className={post.length === 0 ? styles.noPost : styles.displayNone}>
-        <span>진행 중인 거래가 없습니다</span>
-      </div>
-
-      {/* 게시물이 있을때 */}
-      <div
-        className={post.length === 0 ? styles.displayNone : styles.gridWrapper}
-      >
-        {post.map((post) => ShowPost(post, post.id))}
-      </div>
+      {post.length === 0 ? (
+        <div className={styles.noPost}>
+          <span>진행 중인 거래가 없습니다</span>
+        </div>
+      ) : (
+        <div className={styles.gridWrapper}>
+          {post.map((post) => ShowPost(post))}
+        </div>
+      )}
     </div>
   );
 };
